fa6b5142146e854d74f3ae5a69dfab7cbbc82385
jonathanaloya <alo23004@byui.edu>
2024-09-24 20:40:10 +0300

    send

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
-
+const dotenv = require('dotenv');
 const mongodb = require('./data/database');
+
+dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-router.use('/', require('./users'));
+const router = express.Router();
 
-app.use('/', require('./routes'));
+router.use('/', require('./routes/users'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', router);
 
 mongodb.initDb((err) => {
     if (err) {
         console.log(err);
-    }
-    else {
-        app.listen(port, () => {console.log(`Database is running and node Running on port ${port}`)});
+    } else {
+        app.listen(port, () => {console.log(`Database is running and Node is running on port ${port}`)});
     }
 });
-
